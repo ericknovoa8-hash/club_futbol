25d2bc86c58cdb2f226fd23253f91ec0c2684f3f
ericknovoa8-hash <ericknovoa8@gmail.com>
2026-06-06 00:12:16 -0500

    se hizo la coneccion con railwai y docker correctamente

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, role) VALUES ('admin', 'tu_clave_encriptada_o_en_texto', 'ROLE_ADMIN');
+INSERT INTO users (username, password, role) VALUES ('erick', 'otra_clave', 'ROLE_USER');
